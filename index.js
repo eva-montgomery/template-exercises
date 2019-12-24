087056a033da35e9cc902f7e8a21aa4b48655acc
eva montgomery <eva.m.montgomery@gmail.com>
2019-12-24 12:44:41 +0100

    add first exercise

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const http = require('http');
+
+const header = fs.readFileSync('templates/header.html');
+const nav = fs.readFileSync('templates/nav.html');
+const footer = fs.readFileSync('templates/footer.html');
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    const content = `<h1>Hello!</h1><h2>It is ${new Date()}</h2>`;
+    res.end(header + nav + content + footer);
+
+});
+
+server.listen(3000, () => {
+    console.log('Server listening at port 3000');
+})
